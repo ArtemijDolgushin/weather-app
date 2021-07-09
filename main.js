958b699489cc8d050dbec41da1373ff994733300
boo-mer <artemijdolgushin@gmail.com>
2021-07-09 16:52:01 +0300

    .

---
+++ main.js
@@ -0,0 +1,5 @@
+import './modules/css/style.css'
+import './modules/styles/style.css'
+
+import {function1} from "./modules/js/module1";
+function1();
---
+++ modules/js/module1.js
@@ -0,0 +1,7 @@
+
+const function1 = () => {
+    document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>`
+}
+export {function1}
