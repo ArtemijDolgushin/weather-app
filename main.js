8c787acf3f7ff2a6f848123dbc0f935d61f36cdf
boo-mer <artemijdolgushin@gmail.com>
2021-07-09 17:24:39 +0300

    commit

--- main.js
+++ main.js
@@ -1,5 +1,2 @@
-import './modules/css/style.css'
-import './modules/styles/style.css'
+import './styles/style.css'
 
-import {function1} from "./modules/js/module1";
-function1();
--- modules/js/module1.js
+++
@@ -1,7 +0,0 @@
-
-const function1 = () => {
-    document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>`
-}
-export {function1}
